130b7c40522e3ffc94b23ba3e84ab26766ab5ab5
HalaTheAnalyst <140041037+hal00sh@users.noreply.github.com>
2024-01-02 00:01:05 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,84 @@
+import time
+from playsound import playsound
+from threading import Thread
+
+def play_sound():
+    playsound('music.wav')
+
+thread = Thread(target=play_sound)
+thread.start()
+print("♪(┌・。・)┌")
+time.sleep(0.5)
+print("(ノ°ο°)ノ")
+time.sleep(0.6)
+#time delay
+print("ヘ(^_^ヘ)")
+time.sleep(0.42)
+#time delay
+print("°\(^▿^)/°")
+time.sleep(0.3)
+#time delay
+print("└（★ｏ★）┐")
+time.sleep(0.3)
+#time delay
+print("(ノ°ο°)ノ")
+time.sleep(0.3)
+#dance effect animation
+print("ƪ(˘⌣˘)┐ƪ(˘⌣˘)ʃ┌(˘⌣˘)ʃ")
+time.sleep(1.2)
+#time delay
+print("i")
+time.sleep(0.2)
+print("just")
+time.sleep(0.2)
+print("wanna")
+time.sleep(0.2)
+print("Dance!")
+time.sleep(0.60)
+#time delay+ animation effect
+print("〜(￣▽￣〜)(〜￣▽￣)〜")
+time.sleep(1)
+#time delay
+print("can u give me one mo- one mo-")
+time.sleep(1.2)
+#time delay
+print("ヾ(･ω･*)ﾉ♫ ♪ ılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılı ♪ヾ(･ω･*)ﾉ")
+time.sleep(1.5)
+#time delay
+print("         i just wanna dance!         ")
+time.sleep(1.1)
+#time delay
+print("\(._.\\) ƪ(‘-’ ƪ)(ʃ ‘-’)ʃ (/._.)/")
+time.sleep(1)
+print("Baby Give Me One More Chance!")
+time.sleep(1.20)
+print("""(_＼ヽ
+　 ＼＼ .Λ＿Λ.
+　　 ＼(　ˇωˇ)　
+　　　 >　⌒ヽ
+　　　/ 　 へ＼
+　　 /　　/　＼＼
+　　 ﾚ　ノ　　 ヽ_つ
+　　/　/
+　 /　/|
+　(　(ヽ
+　|　|、＼
+　| 丿 ＼ ⌒)
+　| |　　) /
+`ノ ) 　 Lﾉ
+(_／
+    ⠀⠀""")
+time.sleep(0.40)
+print("""
+      ⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢰⣿⡿⠗⠀⠠⠄⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⡜⠁⠀⠀⠀⠀⠀⠈⠑⢶⣶⡄
+⢀⣶⣦⣸⠀⢼⣟⡇⠀⠀⢀⣀⠀⠘⡿⠃
+⠀⢿⣿⣿⣄⠒⠀⠠⢶⡂⢫⣿⢇⢀⠃⠀
+⠀⠈⠻⣿⣿⣿⣶⣤⣀⣀⣀⣂⡠⠊⠀⠀
+⠀⠀⠀⠃⠀⠀⠉⠙⠛⠿⣿⣿⣧⠀⠀⠀
+⠀⠀⠘⡀⠀⠀⠀⠀⠀⠀⠘⣿⣿⡇⠀⠀
+⠀⠀⠀⣷⣄⡀⠀⠀⠀⢀⣴⡟⠿⠃⠀⠀
+⠀⠀⠀⢻⣿⣿⠉⠉⢹⣿⣿⠁⠀⠀⠀⠀
+⠀⠀⠀⠀⠉⠁⠀⠀⠀⠉⠁⠀⠀⠀⠀⠀  
+      """)
